49ef49869f7631a2a06983f72a0a14ab90e220a1
Eirik Tobiassen <eirtob98@hotmail.com>
2021-02-27 22:24:22 +0100

    Client arguments started

--- client/src/main.py
+++ client/src/main.py
@@ -1,7 +1,18 @@
-#!/usr/bin/env python3
-
 import requests
+import argparse
 
-r = requests.get('http://localhost:3000')
+parser = argparse.ArgumentParser(
+    prog = 'selido',
+    description = 'Client to interact with selido server'
+    )
 
-print(r.text)
+subparsers = parser.add_subparsers()
+parser_add = subparsers.add_parser('add', aliases = ['a'], help = 'add items to selido')
+parser_add.add_argument('book')
+parser_get = subparsers.add_parser('get', aliases = ['g'], help = 'get items from selido')
+parser_tag = subparsers.add_parser('tag', aliases = ['t'], help = 'tag items')
+parser_delete = subparsers.add_parser('delete', aliases = ['d', 'del'], help = 'delete items from selido')
+
+args = parser.parse_args()
+
+print(args)
